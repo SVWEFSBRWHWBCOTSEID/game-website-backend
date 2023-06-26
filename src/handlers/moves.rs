a61124273f3994e40a41d293f8dcd9eb970edb5a
Kepler Boyce <keplerboy@gmail.com>
2023-06-25 22:05:04 -0700

    unfinished

--- src/handlers/moves.rs
+++ src/handlers/moves.rs
@@ -1,20 +1,12 @@
 use actix_web::{Error, HttpRequest, HttpResponse};
-use serde::Serialize;
+
+use crate::models::Move;
 
 
-#[derive(Serialize)]
-struct TestResponse {
-    game_id: String,
-    r#move: String,
-}
-
-pub async fn test(req: HttpRequest) -> Result<HttpResponse, Error> {
+// route for adding a move to a game
+pub async fn add_move(req: HttpRequest) -> Result<HttpResponse, Error> {
     let game_id: String = req.match_info().get("game_id").unwrap().parse().unwrap();
-    let r#move: String = req.match_info().query("move").parse().unwrap();
-    let obj = TestResponse {
-        game_id,
-        r#move,
-    };
-    Ok(HttpResponse::Ok().json(obj))
+    let user_move: String = req.match_info().query("move").parse().unwrap();
+    Ok(HttpResponse::Ok().finish())
 }
 
