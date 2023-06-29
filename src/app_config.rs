f31f52e08135d5afaacb1163e134cb139228b82b
Kepler Boyce <keplerboy@gmail.com>
2023-06-29 15:37:59 -0700

    add create user route

--- src/app_config.rs
+++ src/app_config.rs
@@ -1,9 +1,10 @@
 use actix_web::web;
 
-use crate::handlers::game;
+use crate::handlers::{game, user};
 
 
 pub fn config_app(cfg: &mut web::ServiceConfig) {
     cfg
-        .service(game::create_game);
+        .service(game::create_game)
+        .service(user::create_user);
 }
