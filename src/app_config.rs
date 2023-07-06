8316315ad283ea7523774e5a0e31d74be7e53417
Kepler Boyce <keplerboy@gmail.com>
2023-07-06 11:51:37 -0700

    add new endpoint to app config

--- src/app_config.rs
+++ src/app_config.rs
@@ -12,5 +12,6 @@ pub fn config_app(cfg: &mut web::ServiceConfig) {
         .service(user::create_user)
         .service(user::get_user)
         .service(user::login)
-        .service(user::logout);
+        .service(user::logout)
+        .service(sse::new_client);
 }
