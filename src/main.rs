39dc68092481c1d47c7d6b195d9e2ff1498fd0b9
Kepler Boyce <keplerboy@gmail.com>
2023-07-08 02:02:24 -0700

    make session cookies persistent with 3 day expiration

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
 use actix_cors::Cors;
+use actix_session::config::PersistentSession;
 use actix_session::storage::RedisSessionStore;
 use actix_session::SessionMiddleware;
-use actix_web::cookie::{Key, SameSite};
+use actix_web::cookie::{Key, SameSite, time::Duration};
 use actix_web::{middleware, web, App, HttpServer};
 
 use game_backend::app_config::config_app;
 use game_backend::prisma::PrismaClient;
 use game_backend::sse::Broadcaster;
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let client = web::Data::new(PrismaClient::_builder().build().await.unwrap());
-
-    let secret_key = Key::generate();
-    let redis_uri = "redis://127.0.0.1:6379";
-    let redis_store = RedisSessionStore::new(redis_uri).await.unwrap();
-
+    let redis_store = RedisSessionStore::new("redis://127.0.0.1:6379").await.unwrap();
     let broadcaster = Broadcaster::create();
 
     std::env::set_var("RUST_LOG", "debug");
@@ -32,7 +30,10 @@ async fn main() -> std::io::Result<()> {
             .wrap(
                 SessionMiddleware::builder(
                     redis_store.clone(),
-                    secret_key.clone(),
+                    Key::from(&[0; 64]),
+                )
+                .session_lifecycle(
+                    PersistentSession::default().session_ttl(Duration::days(3))
                 )
                 .cookie_same_site(SameSite::None)
                 .build()
