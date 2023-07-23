421082391bded95e2d5868b118df89e69a99309c
Kepler Boyce <keplerboy@gmail.com>
2023-07-23 00:44:59 -0700

    fix get game endpoint to fetch perfs

--- src/handlers/game/get_game.rs
+++ src/handlers/game/get_game.rs
@@ -1,6 +1,6 @@
 use actix_web::{HttpRequest, web::Data, get, HttpResponse};
 
-use crate::helpers::general::get_game_by_id;
+use crate::helpers::general::get_game_with_relations;
 use crate::prisma::PrismaClient;
 use crate::common::WebErr;
 
@@ -13,7 +13,7 @@ pub async fn get_game(
 ) -> Result<HttpResponse, WebErr> {
 
     let game_id: String = req.match_info().get("id").unwrap().parse().unwrap();
-    let game = get_game_by_id(&client, &game_id).await?;
+    let game = get_game_with_relations(&client, &game_id).await?;
 
     Ok(HttpResponse::Ok().json(game.to_game_res(&client).await?))
 }
