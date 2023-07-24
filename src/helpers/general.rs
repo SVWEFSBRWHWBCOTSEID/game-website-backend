56568b6ec7cff7d09b99943af4deafeba890a385
Kepler Boyce <keplerboy@gmail.com>
2023-07-23 23:23:57 -0700

    more error handling

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -14,7 +14,7 @@ use super::game::LobbyVec;
 
 pub fn get_username(session: &Session) -> Result<String, WebErr> {
     match session.get("username") {
-        Ok(u) => Ok(u.unwrap()),
+        Ok(u) => Ok(u.ok_or(WebErr::Unauth(format!("missing session cookie to get username")))?),
         Err(_) => Err(WebErr::Unauth(format!("missing session cookie to get username"))),
     }
 }
@@ -25,7 +25,7 @@ pub async fn get_game_by_id(client: &web::Data<PrismaClient>, id: &str) -> Resul
         .find_unique(game::id::equals(id.to_string()))
         .exec()
         .await
-        .unwrap()
+        .or(Err(WebErr::Internal(format!("error fetching game with id {}", id))))?
     {
         Some(g) => Ok(g),
         None => Err(WebErr::NotFound(format!("could not find game with id {}", id))),
@@ -39,7 +39,7 @@ pub async fn get_game_validate(client: &web::Data<PrismaClient>, id: &str, usern
         .find_unique(game::id::equals(id.to_string()))
         .exec()
         .await
-        .unwrap()
+        .or(Err(WebErr::Internal(format!("error fetching game with id {}", id))))?
     {
         Some(g) => if GameStatus::from_str(&g.status)? != GameStatus::Started ||
             g.first_username.clone().unwrap() != username && g.second_username.clone().unwrap() != username {
@@ -61,7 +61,7 @@ pub async fn get_game_with_relations(client: &web::Data<PrismaClient>, id: &str)
         .with(game::chat::fetch(vec![message::game_id::equals(id.to_string())]))
         .exec()
         .await
-        .unwrap()
+        .or(Err(WebErr::Internal(format!("error fetching game with id {}", id))))?
     {
         Some(g) => Ok(g),
         None => Err(WebErr::NotFound(format!("could not find game with id {}", id))),
@@ -107,7 +107,7 @@ pub async fn get_user_with_relations(client: &web::Data<PrismaClient>, username:
         )
         .exec()
         .await
-        .unwrap()
+        .or(Err(WebErr::Internal(format!("error fetching user {}", username))))?
     {
         Some(u) => Ok(u),
         None => Err(WebErr::NotFound(format!("could not find user {}", username))),
