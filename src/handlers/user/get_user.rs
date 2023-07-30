e1a1b2fa54f8dc4fc1ef03dc382d6fe360325654
Kepler Boyce <keplerboy@gmail.com>
2023-07-29 23:59:06 -0700

    respond with null instead of 404 if user is not found

--- src/handlers/user/get_user.rs
+++ src/handlers/user/get_user.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpRequest, HttpResponse, get};
 
 use crate::common::WebErr;
 use crate::helpers::general::get_user_with_relations;
+use crate::models::res::UserResponse;
 use crate::prisma::PrismaClient;
 
 
@@ -13,7 +14,10 @@ pub async fn get_user(
 ) -> Result<HttpResponse, WebErr> {
 
     let username: String = req.match_info().get("username").unwrap().parse().unwrap();
-    let mut user = get_user_with_relations(&client, &username).await?;
+    let mut user = match get_user_with_relations(&client, &username).await {
+        Ok(u) => u,
+        Err(_) => return Ok(HttpResponse::Ok().json(None::<UserResponse>)),
+    };
     user.update_perfs(&client).await?;
 
     Ok(HttpResponse::Ok().json(user.to_user_res()?))
