556291449cda234032e9f01a69127cd635d6765d
Kevin Yu <kevinyu567@gmail.com>
2023-08-25 19:23:39 -0400

    Stricter ttt move validation

--- src/helpers/moves/ttt.rs
+++ src/helpers/moves/ttt.rs
@@ -3,6 +3,8 @@ use crate::models::general::MoveOutcome;
 
 pub fn validate_ttt_move(moves: Vec<&str>, new_move: &str) -> bool {
     !moves.contains(&new_move)
+        && new_move.chars().nth(0).is_some_and(|c| matches!(c, 'a'..='c'))
+        && new_move.chars().nth(1).is_some_and(|c| c.is_digit(10))
 }
 
 #[derive(PartialEq, Clone)]
