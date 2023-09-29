336749b88a359a4c5d49048abba5b4cfeef8f7b7
Kepler Boyce <keplerboy@gmail.com>
2023-09-29 15:04:38 -0400

    send user message events to both users + fix borrow checker problem

--- src/helpers/conversation.rs
+++ src/helpers/conversation.rs
@@ -5,9 +5,9 @@ impl conversation::Data {
     pub fn to_conversation_res(&self, username: &str) -> ConversationResponse {
         ConversationResponse {
             other_name: if self.username == username {
-                self.other_name
+                self.other_name.clone()
             } else {
-                self.username
+                self.username.clone()
             }
         }
     }
