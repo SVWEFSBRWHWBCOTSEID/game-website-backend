cc6491ba206d7ea6122542e6345d4acf5819b1d4
Kepler Boyce <keplerboy@gmail.com>
2024-02-15 22:57:18 -0500

    testing dockerfile changes

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM rust:1.70.0
 
 WORKDIR /app
-COPY . .
+COPY ./target/release/game-backend .
+COPY ./target/release/prisma-cli .
+COPY ./scripts/docker_commands.sh .
 
 EXPOSE 8080
 EXPOSE 5432
 EXPOSE 6379
 
-ENTRYPOINT ["./scripts/docker_commands.sh"]
+ENTRYPOINT ["./docker_commands.sh"]
--- scripts/docker_commands.sh
+++ scripts/docker_commands.sh
@@ -2,8 +2,8 @@
 
 # Apply migrations, skipping the baseline migration
 # https://www.prisma.io/docs/orm/prisma-migrate/workflows/baselining
-./target/release/prisma-cli migrate resolve --applied 20230930182401_baseline
-./target/release/prisma-cli migrate deploy
+./prisma-cli migrate resolve --applied 20230930182401_baseline
+./prisma-cli migrate deploy
 
 # Run backend executable
-./target/release/game-backend
+./game-backend
