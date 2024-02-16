817a74569ebc6678cfc3205f660e0be612a039c6
Kevin Yu <kevinyu567@gmail.com>
2024-02-15 23:02:16 -0500

    Revert "testing dockerfile changes"
    This reverts commit cc6491ba206d7ea6122542e6345d4acf5819b1d4.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
 FROM rust:1.70.0
 
 WORKDIR /app
-COPY ./target/release/game-backend .
-COPY ./target/release/prisma-cli .
-COPY ./scripts/docker_commands.sh .
+COPY . .
 
 EXPOSE 8080
 EXPOSE 5432
 EXPOSE 6379
 
-ENTRYPOINT ["./docker_commands.sh"]
+ENTRYPOINT ["./scripts/docker_commands.sh"]
--- scripts/docker_commands.sh
+++ scripts/docker_commands.sh
@@ -2,8 +2,8 @@
 
 # Apply migrations, skipping the baseline migration
 # https://www.prisma.io/docs/orm/prisma-migrate/workflows/baselining
-./prisma-cli migrate resolve --applied 20230930182401_baseline
-./prisma-cli migrate deploy
+./target/release/prisma-cli migrate resolve --applied 20230930182401_baseline
+./target/release/prisma-cli migrate deploy
 
 # Run backend executable
-./game-backend
+./target/release/game-backend
